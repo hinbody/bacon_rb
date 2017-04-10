204dd95cc7d98e952c9a10f88dd528cd270855c9
Harley Inbody <hinbody@gmail.com>
2017-04-09 23:43:12 -0400

    Initial commit accepts initial members of an entry, and accepts an amount paid to a creditor for a specific billing date

---
+++ owed.rb
@@ -0,0 +1,24 @@
+# This script will accept input of what is owed, and store it in a database. The
+# idea is to eventually be able to accept this input and store it, then look for
+# trends or search for a particular piece of data, whether that be from a
+# specific time, or a specific or estimated dollar amount. i.e., what bills over
+# the last two years were consistently over $200. What was my average amount
+# spent on [utilites, fuel, dinners, entertainment, etc...] over the last year
+
+creditor = ARGV.first
+prompt = '> '
+
+print "Enter the following for #{creditor}:\n"
+print "Bill date #{prompt}"
+bill_date = $stdin.gets.chomp
+print "Due date #{prompt}"
+due_date = $stdin.gets.chomp
+print "Current amount due #{prompt}"
+current_due = $stdin.gets.chomp
+
+puts """
+The current details for #{creditor} are as follows:
+The current billing date is => #{bill_date}
+The current due date is => #{due_date}
+The current amount due is => #{current_due}
+"""
---
+++ paid.rb
@@ -0,0 +1,10 @@
+# This script will allow input of payments of bills. Then this information will
+# be stored in a database to be tracked or queried later
+
+creditor, bill_date = ARGV
+
+puts "What is the amount you are paying #{creditor} for billing date #{bill_date}? "
+print "Amount: "
+amount_paid = $stdin.gets.chomp.to_f
+
+puts "You have paid #{amount_paid.round(2)} to #{creditor} for billing date #{bill_date}"
